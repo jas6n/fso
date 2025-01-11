caa98b0c964b2a8506a4cd3061c7298e0649febc
jas6n <73123996+jas6n@users.noreply.github.com>
2025-01-11 18:23:57 -0500

    completed forms

--- p2/part2/src/App.jsx
+++ p2/part2/src/App.jsx
@@ -1,39 +1,50 @@
-// import Note from './components/Note'
+import {useState} from 'react'
+import Note from './components/Note'
 
-// const App = ({notes}) => {
+const App = (props) => {
 
-//   return (
-//     <div>
-//       <h1>Notes</h1>
-//       <ul>
-//         {notes.map(note =>
-//           <Note key={note.id} note={note} />
-//         )}
-//       </ul>
-//     </div>
-//   )
-// }
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState()
+  const [showAll, setShowAll] = useState(true)
 
-// export default App
-
-const Course = ({ props }) => {
-  console.log(props)
-  const { course } = props
-  return (<div>
-    <Header course={course} />
-  </div>)
-}
-
-const App = () => {
-  const course = {
-    // ...
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    }
+  
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
   }
 
-  console.log("app works")
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+  ? notes
+  : notes.filter(note => note.important) // if show all is true then notesToShow is notes
 
   return (
     <div>
-      <Course course={course} />
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form> 
     </div>
   )
 }
--- p2/part2/src/components/Note.jsx
+++ p2/part2/src/components/Note.jsx
@@ -1,10 +1,7 @@
-const Note = ({note}) => {
-
-    return (
-      <li>
-        {note.content}
-      </li>
-    )
-  }
+const Note = ({ note }) => {
+  return (
+    <li>{note.content}</li>
+  )
+}
 
 export default Note
